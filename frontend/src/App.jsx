ab5466cb2c557f8dbd5e9f0a1518437183a421c2
Jemuel Malaga <malagajemuel@gmail.com>
2025-09-03 08:37:42 +0800

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+import Title from './Title'
+import Nav from './Nav'
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <Title name="this is my post" subTitle="this is a subtitle2" />
+    </>
+  )
+}
+
+export default App
