cf52965ecb8440a583943e99983a336026cb14ca
Jemuel Malaga <malagajemuel@gmail.com>
2025-09-04 14:23:14 +0800

    initiate models, cloudinary api

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,13 @@ const app = express();
 const cors = require('cors')
 const postRoutes = require('./routes/post');
 const authRoutes = require('./routes/auth');
+const cookieParser = require('cookie-parser')
 
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit: "50mb", extended: true }));
+app.use(cookieParser());
 app.use(cors());
+
 
 app.use('/api/v1', postRoutes);
 app.use('/api/v1', authRoutes);
