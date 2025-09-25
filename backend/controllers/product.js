3b0fea33cb0720c3c0dfb0e8b161381176e55cc2
Jemuel Malaga <malagajemuel@gmail.com>
2025-09-25 15:01:07 +0800

    pagination and filter

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,4 +1,3 @@
-
 const Product = require('../models/product')
 const Order = require('../models/order');
 
@@ -149,8 +148,22 @@ exports.getProducts = async (req, res) => {
     const productsCount = await Product.countDocuments();
 
     // const products = await Product.find()
-    const apiFeatures = new APIFeatures(Product.find(), req.query).search()
-    const products = await apiFeatures.query;
+    const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter()
+    
+    apiFeatures.pagination(resPerPage);
+	const products = await apiFeatures.query;
+    let filteredProductsCount = products.length;
+
+    if (!products)
+        return res.status(400).json({ message: 'error loading products' })
+    return res.status(200).json({
+        success: true,
+        products,
+        filteredProductsCount,
+        resPerPage,
+        productsCount,
+
+    })
     if (!products)
         return res.status(400).json({ message: 'error loading products' })
     
